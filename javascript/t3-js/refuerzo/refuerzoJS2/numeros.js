c19d0baee50a5d9d439ad0545dfc0ed092365986
José García Torrescusa <josegarcia05@iesamachado.org>
2025-01-23 23:53:17 +0100

    Terminar en clase

---
+++ javascript/t3-js/refuerzo/refuerzoJS2/numeros.js
@@ -0,0 +1,44 @@
+//numeros.html
+var lista = []
+
+//Añadir números
+function anadir()
+{
+    var numero = parseFloat(document.getElementById("num").value);
+    //Comprobar número
+    if (isNaN(numero))
+    {
+        alert("Por favor introduzca valores numéricos");
+        return;
+    }
+    //Añadir valores a la lista
+    lista.push(numero);
+    //Titulo indicando que número se ha añadido a la lista
+    document.getElementById("ultimo").innerHTML = "Último nº añadido: " +numero;
+}
+
+//Sumar números de la lista
+function suma()
+{
+    var total = 0;
+    //Bucle que automatice la suma
+    for (var i=0; i < lista.length; i++)
+    {
+        total = total + lista[i]
+    }
+    //Título indicando la suma
+    document.getElementById("total").innerHTML = "La suma total de la lista es: " +total;
+}
+
+//Encontrar máximo en la lista
+function mayor()
+{
+    var maximo;
+    for (var i=0; 1 < lista.length; i++)
+    {
+        maximo=Math.max(lista[i]);
+    }
+    //Título indicando el nº máximo
+    document.getElementById("maximo").innerHTML = "El número más alto es:" +maximo;
+
+}
